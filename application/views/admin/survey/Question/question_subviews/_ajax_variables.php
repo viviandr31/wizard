63e800ea93fc16613a98237cb87181996b0e87f5
Vivian <viviandr31@gmail.com>
2018-10-22 10:41:56 -0700

    initial commit

---
+++ application/views/admin/survey/Question/question_subviews/_ajax_variables.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This view render the needed variables for the ajax process of the creation of a new question
+ */
+
+App()->getClientScript()->registerScript("EditQuestionView_basic_variables" ,"
+    var attr_url = '".$this->createUrl('admin/questions', array('sa' => 'ajaxquestionattributes'))."';
+    var imgurl = '".Yii::app()->getConfig('imageurl')."';
+    var validateUrl = '".$sValidateUrl."';
+    var questionTypeArray = ".$qTypeOutput.";
+    var selectormodeclass = '".$selectormodeclass."';"
+    , LSYii_ClientScript::POS_BEGIN );
+?>
